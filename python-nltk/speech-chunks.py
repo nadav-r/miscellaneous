18db613e77bb932128fb8051aebaaef5d023b168
nadav <nadavra@gmail.com>
2019-08-21 03:38:28 -0700

    nltk

--- python-nltk/speech-chunks.py
+++ python-nltk/speech-chunks.py
@@ -3,6 +3,9 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import inaugural
 from nltk.stem import WordNetLemmatizer
 import random
+import pprint
+import re
+
 
 speech = inaugural.raw('2013-Obama.txt')
 tokenizedSpeech = sent_tokenize(speech)
@@ -10,15 +13,21 @@ rndIndex = random.randint(0, len(tokenizedSpeech))
 rndSent = tokenizedSpeech[rndIndex]
 print('index: ', rndIndex)
 
+
 def process_sent(sent):
     words = word_tokenize(sent)
     taggedWords = nltk.pos_tag(words)
-    chunkGrammer = r"""Chunk: {<PRP\$>*<JJ.>*<NN.?>+}"""
+    chunkGrammer = r"""Chunk: {<DT>?<JJ>*<NN>}
+                    {<PRP.?>?<CD>?<JJ.?>?<NN.?>}
+                    {<PRP>}"""
     chunkParser = nltk.RegexpParser(chunkGrammer)
     chunked = chunkParser.parse(taggedWords)
-    print(sent, end= '\n--------\n')
-    print(taggedWords, end= '\n--------\n')
-    print(chunked, end= '\n--------\n')
-    chunked.draw()
+    #print(taggedWords, end= '\n--------\n')
+    #print(chunked, end= '\n--------\n')
+    # chunked.draw()
+    nltk.app.chartparser_app.app()
 
-process_sent(rndSent)
+
+# for sent in tokenizedSpeech:
+#     process_sent(sent)
+nltk.app.chartparser_app.app()
