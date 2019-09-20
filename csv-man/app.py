97a3fc9a9d51065f54bbee13fcf5486c059d6061
nadav <nadavra@gmail.com>
2019-09-20 03:34:15 -0700

    show table on index

--- csv-man/app.py
+++ csv-man/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask import render_template
+import pandas as pd
+import sqlite3
+
 app = Flask(__name__)
+
+# c = conn.cursor()
+# c.execute('select * from albums')
+# res = c.fetchfirst()
+# print(res)
+# c.close()
+
+
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    conn = sqlite3.connect('/home/nadav/Desktop/db_example/chinook.db')
+    df = pd.read_sql_query("select * from albums limit 5;", conn)
+    data=df.to_html()
+    return render_template('index.html',data=df)
+
